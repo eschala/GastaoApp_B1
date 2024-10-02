df06860d6cd513210011704864c29480788a5938
eschala <eduar.chala28@outlook.com>
2024-10-02 11:36:56 -0500

    npm i html5-qrcode

--- src/App/GastaoApp.tsx
+++ src/App/GastaoApp.tsx
@@ -1,10 +1,5 @@
-/* GastaoApp.tsx  renderizada por App.tsx */
-/*    <App>
-        <BrowserRouter>
-            <Layout></Layout>
-        <BrowserRouter/>
-      <App/>
- */
+
+
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -13,17 +8,22 @@ import { BrowserRouter } from "react-router-dom";
 import Layout from "./GastaoApp/2. Views/0. Layout/Layout";
 import { darkTheme } from './GastaoApp/1. Models/Themes/Themes';
 
+
 export function GastaoApp() {
   return (
     <>
       <BrowserRouter >
         <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Layout >
-          </Layout>
+          <>
+            <CssBaseline />
+            <Layout >
+
+            </Layout>
+          </>
         </ThemeProvider>
       </BrowserRouter>
 
     </>
   );
 }
+
