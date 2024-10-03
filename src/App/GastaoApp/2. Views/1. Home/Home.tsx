46767f5a708718fda828e22eab42160f04d29953
eschala <eduar.chala28@outlook.com>
2024-10-03 07:36:23 -0500

    npm install --save react-qr-scanner

--- src/App/GastaoApp/2. Views/1. Home/Home.tsx
+++ src/App/GastaoApp/2. Views/1. Home/Home.tsx
@@ -1,5 +1,9 @@
 /* import { QR_Reader } from "../../../scannerQR"; */
 
+import QrReader from "../../../scannerQR";
+
+
+
 
 export const Home = () => {
     return (<>
@@ -10,6 +14,7 @@ export const Home = () => {
             </h3>
             <div className="" style={{ backgroundColor: "gray" }}>
                 {/* <QR_Reader /> */}
+                <QrReader />
             </div>
 
         </div>
