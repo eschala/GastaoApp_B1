2739597807ae9a5628c894c11247e3894a6fd36d
Eduar Samir <eduar.chala28@outlook.com>
2025-07-17 19:20:24 -0500

    Detalles de la home page

---
+++ 3. FRONTEND/gastao-front/src/Application/GastaoApp.tsx
@@ -0,0 +1,11 @@
+export function GastaoApp() {
+
+
+    return (<>
+        <div>
+            <h1>Welcome to Gastao App</h1>
+            <p>This is the main application component.</p>
+        </div>
+    </>
+    )
+}
